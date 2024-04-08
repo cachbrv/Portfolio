9a3a66a387db054b0adffec44a44a2c9553c972b
Camila Chabane <camila.chabane@epitech.eu>
2024-04-08 11:07:04 +0200

    New - added navbar active/not active onscroll logic

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ViewportScroller } from "@angular/common";
 
@@ -11,9 +11,60 @@ import { ViewportScroller } from "@angular/common";
 })
 
 export class NavbarComponent {
-  constructor(private scroller: ViewportScroller) {}
-  
+  constructor(private scroller: ViewportScroller) { }
+  @HostListener('window:scroll', ['$event'])
+
+  onScroll(event: Event) {
+    const region: NodeListOf<HTMLElement> = document.querySelectorAll(".region");
+    const active: string = this.getActiveState();
+    var newActive: string = "";
+
+    region.forEach((reg) => {
+      let top = window.scrollY;
+      let offset = reg.offsetTop - 200;
+      let height = reg.offsetHeight;
+      let id = reg.getAttribute("id");
+
+      if (top >= offset && top < offset + height) {
+        newActive = id!;
+      }
+    });
+
+    if (newActive != "" && active != newActive) {
+      this.removingAllActiveState();
+      const targetElement = document.getElementById(newActive.replace('-nav', ''));
+      targetElement?.classList.add('active');
+    }
+  }
+
   ScrollIntoView(target: string) {
-    this.scroller.scrollToAnchor(target);
+    // Removing active state
+    this.removingAllActiveState();
+
+    // Adding active state to selected target
+    const targetElement = document.getElementById(target);
+    targetElement?.classList.add('active');
+
+    this.scroller.scrollToAnchor(target + '-nav');
+  }
+
+  getActiveState() {
+    var result = "";
+    var els = document.getElementsByClassName("nav-item");
+
+    Array.prototype.forEach.call(els, function (el) {
+      if (el.classList.contains('active')) {
+        result = el.id;
+      }
+    });
+
+    return result;
+  }
+
+  removingAllActiveState() {
+    var els = document.getElementsByClassName("nav-item");
+    Array.prototype.forEach.call(els, function (el) {
+      el.classList.remove('active');
+    });
   }
 }
